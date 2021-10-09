f4741fb0547ce2aeebf78f4f315ecb80a4c7f1c9
david-pinales <david.pinales@gmail.com>
2021-10-09 19:53:51 +0000

    Initial push for Web Development

---
+++ Project5/script.js
@@ -0,0 +1,25 @@
+
+
+var imgArray = [
+    "assets/desserts1.jpg", "assets/desserts2.jpg", "assets/desserts3.jpg", "assets/desserts4.jpg", "assets/dessert5.jpg", "assets/dessert6.jpg"
+]
+
+var index = 0;
+var indexReverse = index.reverse();
+function previous(){
+    document.querySelector("img").setAttribute("src", imgArray[indexReverse]);
+    index++;
+    if (indexReverse >= imgArray.length){
+        index = 0;
+    }
+}
+
+function next(){
+    document.querySelector("img").setAttribute("src", imgArray[index]);
+    index++;
+    if (index >= imgArray.length){
+        index = 0;
+    }
+}
+    
+    
